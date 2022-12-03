fd80ece243cadafdf5a4092b6f049b306b6ffbd1
IanTaiAhn <iantaiahn@mail.weber.edu>
2022-12-03 00:31:57 -0700

    Epic Sauce.

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,10 @@
-// I'm gonna use some javascript on the menu for small screens!
 document.getElementById("menu").addEventListener("click", (e) => {
   e.target.nextElementSibling.classList.toggle("translate-x-full");
   e.target.nextElementSibling.classList.add("min-w-full");
   e.target.nextElementSibling.classList.add("ease-out");
-  console.log("didit");
 });
+for (el of document.getElementsByClassName("close")) {
+  el.addEventListener("click", (e) => {
+    document.getElementById("closeHam").classList.toggle("translate-x-full");
+  });
+}
