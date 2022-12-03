41ff6f980544975da4eb4e01e27cae4d63811215
IanTaiAhn <iantaiahn@mail.weber.edu>
2022-12-02 23:33:15 -0700

    Breathing new life into this website, and my Tailwind knowledge has vastly increased.

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+// I'm gonna use some javascript on the menu for small screens!
+document.getElementById("menu").addEventListener("click", (e) => {
+  e.target.nextElementSibling.classList.toggle("translate-x-full");
+  e.target.nextElementSibling.classList.add("min-w-full");
+  e.target.nextElementSibling.classList.add("ease-out");
+  console.log("didit");
+});
